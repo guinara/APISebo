86a99716fdc246e7dcbb1b78d62bd258457a30fc
Guilherme <gnara15@gmail.com>
2023-09-30 14:49:28 -0300

    Primeiro commit

---
+++ TODO.py
@@ -0,0 +1,23 @@
+# URL BASE - LOCALHOST
+# ENDPOINTS
+# Campos sugeridos:
+# • ID, Nome, Email, Senha (criptografada), Status (ativo/inativo), Tipo (comprador/vendedor/administrador)
+# Endpoints:
+# • POST /users/signup - Registro do usuário.
+# • POST /users/login - Autenticação do usuário.
+# • PUT /users/{id} - Editar informações do usuário.
+# • DELETE /users/{id} - Desativar um usuário (soft delete). 
+
+
+
+# Campos sugeridos:
+# • ID, Título, Autor, Categoria (livro, revista, etc.), Preço, Breve descrição ou história, Status
+# (ativo/inativo ou em estoque/fora de estoque), Data de edição, Periodicidade, ID do vendedor
+# Endpoints:
+# • POST /items - Adicionar um novo item.
+# • GET /items - Listar todos os itens.
+# • GET /items/{id} - Obter detalhes de um item específico.
+# • PUT /items/{id} - Editar um item.
+# • DELETE /items/{id} - Desativar um item (soft delete).
+# • GET /items/search - Buscar itens por critério
+
