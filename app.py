19af3acc69c37e6ac6767ec2fcdce54fe3422a48
Guilherme <gnara15@gmail.com>
2023-10-04 23:42:15 -0300

    add criptografia e prototipo login

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 import pymongo
 from pymongo.server_api import ServerApi
 from bson import json_util
+import bcrypt
+
 import urllib.parse
 
 
@@ -43,10 +45,36 @@ def edit_users_by_id(id):
 @app.route('/users/signup', methods=['POST'])
 def insert_new_user():
     """Insere um novo usuário no MongoDB"""
-
     new_user = request.get_json()
-    db.Users.insert_one(new_user)
-    return parse_json({"message": "Usuario inserido com sucesso"})
+    contador = get_contador()
+    nome = new_user["Nome"]
+    senha_nao_cripto = new_user["Senha"]
+    salt = bcrypt.gensalt()
+    senha_cripto = bcrypt.hashpw(senha_nao_cripto.encode('utf-8'), salt)
+    senha_cripto = senha_cripto.decode('utf8').replace("'", '"')
+    email = new_user["E-mail"]
+    tipo = new_user["Tipo"]
+    dicionario = {'id': contador, 'E-mail': email, 'Nome': nome,
+                  'Senha': senha_cripto, 'Status': 'Ativado', 'Tipo': tipo}
+    usuario = json.dumps(dicionario, indent=4)
+    db.Users.insert_one(dicionario)
+    return usuario
+
+
+@app.route('/users/login', methods=['GET'])
+def login_user():
+    """Insere um novo usuário no MongoDB"""
+    login = request.get_json()
+    mail = login["E-mail"]
+    try:
+        user = db.Users.find({"E-mail": mail})[0]
+        if user != None:
+            passou = bcrypt.checkpw(login["Senha"].encode('utf-8'),
+                                    user["Senha"].encode('utf-8'))
+            if passou:
+                return "Usuário logado"
+    except:
+        return "Não achou"
 
 
 @app.route('/users/<int:id>', methods=['DELETE'])
@@ -54,6 +82,31 @@ def delete_user(id):
     """Exclui um usuário por ID no MongoDB"""
     db.Users.update_one({"id": id}, {"$set": {"Status": "Desativado"}})
     return parse_json({"message": "Usuario excluído com sucesso"})
+
+
+def get_contador():
+    """Pega o id de contagem"""
+    conts = db.counters.find()[0]
+    contador = conts["seq_value"]
+    return contador
+
+
+@app.route('/users/teste', methods=['POST'])
+def teste_insert():
+    """Insere um novo usuário no MongoDB"""
+    new_user = request.get_json()
+    contador = get_contador()
+    nome = new_user["Nome"]
+    senhaNaoCripto = new_user["Senha"]
+    salt = bcrypt.gensalt()
+    senha_cripto = bcrypt.hashpw(senhaNaoCripto.encode('utf-8'), salt)
+    senha_cripto = senha_cripto.decode('utf8').replace("'", '"')
+    email = new_user["E-mail"]
+    tipo = new_user["Tipo"]
+    dicionario = {'id': contador, 'E-mail': email, 'Nome': nome,
+                  'Senha': senha_cripto, 'Status': 'Ativado', 'Tipo': tipo}
+    usuario = json.dumps(dicionario, indent=4)
+    return usuario
 
 
 @app.route('/')
