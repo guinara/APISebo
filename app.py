b109d87e218f0e1344409849f53315e8f885d2ad
Guilherme Nara de Souza Stambonsky <61251265+guinara@users.noreply.github.com>
2023-11-27 19:31:41 -0300

    Update app.py
    remoção comentarios

--- app.py
+++ app.py
@@ -8,24 +8,7 @@ from routes.categorias import categorias
 
 
 app = Flask(__name__)
-# api = Api(app)
-# swagger = Swagger(app)
 
-
-# class InicioAPI(Resource):
-#     def get(self):
-#         """
-#         Inicio da api com a documentação
-#         ---
-#         responses:
-#           200:
-#             description: OK
-#         """
-#         return {'hello': 'world'}
-
-
-# api.add_resource(InicioAPI, '/inicio')
-# app.secret_key = os.environ.get('secret_key')
 app.secret_key = "SenhaSecretaUOOOOU"
 
 app.register_blueprint(users, url_prefix='/users')
